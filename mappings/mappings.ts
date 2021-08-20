504a513fc3e78db7c9cfa09fa927a9e1261eac3a
Vladyslav Samchuk <sam4uk.v1ad@gmail.com>
2021-08-20 19:21:15 +0300

    Add reject immediately json file
    Use bignumber.js lib

--- mappings/mappings.ts
+++ mappings/mappings.ts
@@ -1,6 +1,10 @@
 import { EventContext, StoreContext } from '@joystream/hydra-common'
 import { Treasury } from './generated/types'
 import { KusamaProposals, Status } from '../generated/graphql-server/src/modules/kusama-proposals/kusama-proposals.model';
+import { readFileSync } from 'fs'
+import BN from 'bignumber.js'
+
+const getRejectedProposalFromFile = () => JSON.parse(readFileSync(`${__dirname}/../../rejectedImmediately.json`, 'utf-8')).proposalIndexes
 
 export async function treasuryProposed({ event, store }: EventContext & StoreContext) {
   const [ proposalIndex ] = new Treasury.ProposedEvent(event).params
@@ -8,6 +12,9 @@ export async function treasuryProposed({ event, store }: EventContext & StoreCon
   if (event.extrinsic === undefined) {
     throw new Error(`No extrinsic has been provided`)
   }
+
+  const rejectedPropId: number[] = getRejectedProposalFromFile()
+  const isRejected = rejectedPropId.includes(proposalIndex.toNumber())
 
   const [requestedAmount, beneficiary] = event.extrinsic.args
   if (event.extrinsic.args[0].name === 'threshold') return
@@ -21,8 +28,8 @@ export async function treasuryProposed({ event, store }: EventContext & StoreCon
   proposal.requestedAmount = requestedAmount.value.toString()
   proposal.proposer = event.extrinsic.signer
   proposal.beneficiary = beneficiaryAccount
-  proposal.boundedAmount = (Number(requestedAmount.value) * 0.05).toString()
-  proposal.status = Status.Proposed
+  proposal.boundedAmount = (new BN(requestedAmount.value.toString()).multipliedBy(new BN(0.05))).toString()
+  proposal.status = !isRejected ? Status.Proposed : Status.Rejected
   store.save(proposal)
 }
 
